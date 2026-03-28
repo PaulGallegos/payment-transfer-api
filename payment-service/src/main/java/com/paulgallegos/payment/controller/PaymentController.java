c6c7d92e69f28baa0ee809058d2e20c58ae60250
Paul Gallegos <pagallegos13@gmail.com>
2026-03-28 17:54:49 -0600

    feat: add getPayment and getPayemntHistory funcionality

--- payment-service/src/main/java/com/paulgallegos/payment/controller/PaymentController.java
+++ payment-service/src/main/java/com/paulgallegos/payment/controller/PaymentController.java
@@ -5,7 +5,12 @@ import com.paulgallegos.payment.dto.PaymentResponse;
 import com.paulgallegos.payment.service.PaymentService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -26,6 +31,16 @@ public class PaymentController {
     @ResponseStatus(HttpStatus.CREATED)
     public PaymentResponse createPayment(@RequestHeader("X-User-Id") UUID userId, @Valid @RequestBody CreatePaymentRequest paymentRequest){
         return paymentService.createPayment(userId, paymentRequest);
+    }
+
+    @GetMapping("/{paymentId}")
+    public PaymentResponse getPayment(@RequestHeader("X-User-Id") UUID userId, @PathVariable("paymentId") UUID paymentId){
+        return paymentService.getPayment(userId, paymentId);
+    }
+
+    @GetMapping("/history")
+    public Page<PaymentResponse> getPaymentHistory(@RequestHeader("X-User-Id") UUID userId, @PageableDefault(size = 20, sort = "createdAt") Pageable pageable){
+        return paymentService.getPaymentHistory(userId, pageable);
     }
 
 
--- payment-service/src/main/java/com/paulgallegos/payment/service/PaymentService.java
+++ payment-service/src/main/java/com/paulgallegos/payment/service/PaymentService.java
@@ -5,8 +5,11 @@ import com.paulgallegos.payment.domain.PaymentStatus;
 import com.paulgallegos.payment.dto.CreatePaymentRequest;
 import com.paulgallegos.payment.dto.PaymentResponse;
 import com.paulgallegos.payment.exception.DuplicatePaymentException;
+import com.paulgallegos.payment.exception.PaymentNotFoundException;
 import com.paulgallegos.payment.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -42,16 +45,42 @@ public class PaymentService {
 
          Payment paymentSaved =  paymentRepository.saveAndFlush(payment);
 
-         return PaymentResponse.builder()
-                 .id(paymentSaved.getId())
-                 .senderAccountId(paymentSaved.getSenderAccountId())
-                 .receiverAccountId(paymentSaved.getReceiverAccountId())
-                 .amount(paymentSaved.getAmount())
-                 .currency(paymentSaved.getCurrency())
-                 .status(paymentSaved.getStatus())
-                 .description(paymentSaved.getDescription())
-                 .createdAt(paymentSaved.getCreatedAt())
-                 .build();
+         return toResponse(paymentSaved);
+    }
+
+    @Transactional(readOnly = true)
+    public PaymentResponse getPayment(UUID requestingUserId, UUID paymentId){
+
+        Payment payment = paymentRepository.findById(paymentId).orElseThrow(() -> new PaymentNotFoundException(paymentId));
+
+        if(!payment.getSenderAccountId().equals(requestingUserId)){
+            throw new PaymentNotFoundException(paymentId);
+        }
+
+        return toResponse(payment);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<PaymentResponse> getPaymentHistory(UUID senderAccountId, Pageable pageable){
+
+       Page<Payment> paymentPage = paymentRepository.findBySenderAccountId(senderAccountId,pageable);
+
+        return paymentPage.map(this::toResponse);
+    }
+
+
+    private PaymentResponse toResponse(Payment payment){
+        return  PaymentResponse.builder()
+                .id(payment.getId())
+                .senderAccountId(payment.getSenderAccountId())
+                .receiverAccountId(payment.getReceiverAccountId())
+                .status(payment.getStatus())
+                .currency(payment.getCurrency())
+                .amount(payment.getAmount())
+                .description(payment.getDescription())
+                .failureReason(payment.getFailureReason())
+                .createdAt(payment.getCreatedAt())
+                .build();
     }
 
 }
