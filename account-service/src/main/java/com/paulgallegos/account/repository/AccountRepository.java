1f48eac180f057ba84a85061dc5838811533002b
Paul Gallegos <pagallegos13@gmail.com>
2026-03-31 17:42:14 -0600

    feat: add repository

---
+++ account-service/src/main/java/com/paulgallegos/account/repository/AccountRepository.java
@@ -0,0 +1,15 @@
+package com.paulgallegos.account.repository;
+
+import com.paulgallegos.account.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, UUID> {
+    Optional<Account> findByUserId(UUID userId);
+    Optional<Account> findByAccountNumber(String accountNumber);
+    boolean existsByUserId(UUID userId);
+}
